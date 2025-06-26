6b9877772974e051fad076c8afc421fa7143942e
SamuelCV06 <samuel.chinchilla@ucr.ac.cr>
2025-06-26 16:43:15 -0600

    Estadisticas

--- '''Trabajo en grupo avance por listas.py
+++ '''Trabajo en grupo avance por listas.py
@@ -13,8 +13,10 @@ Algoritmo
 Text = input("Ingrese el directorio del texto (hijueputa):")
 with open(Text, 'r') as archivo:
     lineas = archivo.readlines()
+
 import matplotlib.pyplot as plt
 import numpy as np
+import statistics as stats
 #creamos una lista para las 6 palabras
 print("ingrese 6 palabras: ")
 palabra1 = input()
@@ -23,13 +25,14 @@ palabra3 = input()
 palabra4 = input()
 palabra5 = input()
 palabra6 = input()
-n_lineas = []
+n_lineas = [1,2,3,4]
 P1 = [] 
 P2 = [] 
 P3 = []
 P4 = []
 P5 = []
 P6 = []
+a = 1
 #recorremos las listas
 for num_linea, linea in enumerate(lineas, start=1):
     if palabra1 in linea or palabra2 in linea or palabra3 in linea or palabra3 in linea or palabra4 in linea or palabra5 in linea or palabra6 in linea:
@@ -39,17 +42,28 @@ for num_linea, linea in enumerate(lineas, start=1):
         P4.append(linea.count(palabra4))
         P5.append(linea.count(palabra5))
         P6.append(linea.count(palabra6))
-        n_lineas = (num_linea)
 
 
 print(n_lineas)
+
 print(P1, P2, P3, P4, P5, P6)
-n1 = P1[0]
-n2 = P1[-1]
+media = stats.mean(P1)
+mediana = stats.median(P1)
+moda = stats.mode(P1)
+print ("ESTADISTICAS DE PALABRAS,\n", "Palabra 1:", palabra1, "\n", "Total de ocurrencias:", sum(P1), "\n", "MAXIMA cantidad de ocurrencias:", max(P1), "\n", "MINIMA cantidad de ocurrencias:", min(P1), "\n" " Media de ocurrencias:", media , "\n","Mediana de ocurrencias:",mediana,"\n", "Moda de ocurrencias:",moda, "\n")
+"""
+P1.reverse()
+print(P1)
+O1 = int(P1[0])
+O2 = int(P1[1])
+O3 = int(P1[2])
+O4 = int(P1[3])
 
 
-x = np.array([0 , n_lineas])
-y = np.array([n2, n1])
+x = np.array([n_lineas])
+y = np.array([P1])
 plt.plot(x,y)
+
 plt.title(palabra1)
 plt.show()
+"""
