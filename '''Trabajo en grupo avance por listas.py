4578760a8d79891591e5a38626fc7c4dc37fb0bf
SamuelCV06 <samuel.chinchilla@ucr.ac.cr>
2025-06-26 17:43:03 -0600

    Estadisticas en archivo

--- '''Trabajo en grupo avance por listas.py
+++ '''Trabajo en grupo avance por listas.py
@@ -50,10 +50,15 @@ print(P1, P2, P3, P4, P5, P6)
 media = stats.mean(P1)
 mediana = stats.median(P1)
 moda = stats.mode(P1)
-print ("ESTADISTICAS DE PALABRAS,\n", "Palabra 1:", palabra1, "\n", "Total de ocurrencias:", sum(P1), "\n", "MAXIMA cantidad de ocurrencias:", max(P1), "\n", "MINIMA cantidad de ocurrencias:", min(P1), "\n" " Media de ocurrencias:", media , "\n","Mediana de ocurrencias:",mediana,"\n", "Moda de ocurrencias:",moda, "\n")
-
-
-
+""
+with open("Estadísticas.txt", "w",) as E:
+    E.write(f"ESTADISTICAS DE PALABRAS \n Palabra 1: {palabra1} \n Total de ocurrencias: {sum(P1)} \n MAXIMA cantidad de ocurrencias: {max(P1)} \n MINIMA cantidad de ocurrencias: {min(P1)} \n Media de ocurrencias: {stats.mean(P1)} \n Mediana de ocurrencias {stats.median(P1)} \n Moda de ocurrencias:{stats.mode(P1)} \n\n ")
+with open("Estadísticas.txt", "a") as E: 
+    E.write(f"Palabra 2: {palabra2} \n Total de ocurrencias: {sum(P2)} \n MAXIMA cantidad de ocurrencias: {max(P2)} \n MINIMA cantidad de ocurrencias: {min(P2)} \n Media de ocurrencias: {stats.mean(P2)} \n Mediana de ocurrencias {stats.median(P2)} \n Moda de ocurrencias:{stats.mode(P2)} \n\n")
+    E.write(f"Palabra 3: {palabra3} \n Total de ocurrencias: {sum(P3)} \n MAXIMA cantidad de ocurrencias: {max(P3)} \n MINIMA cantidad de ocurrencias: {min(P3)} \n Media de ocurrencias: {stats.mean(P3)} \n Mediana de ocurrencias {stats.median(P3)} \n Moda de ocurrencias:{stats.mode(P3)} \n\n")
+    E.write(f"Palabra 4: {palabra4} \n Total de ocurrencias: {sum(P4)} \n MAXIMA cantidad de ocurrencias: {max(P4)} \n MINIMA cantidad de ocurrencias: {min(P4)} \n Media de ocurrencias: {stats.mean(P4)} \n Mediana de ocurrencias {stats.median(P4)} \n Moda de ocurrencias:{stats.mode(P4)} \n\n")
+    E.write(f"Palabra 5: {palabra5} \n Total de ocurrencias: {sum(P5)} \n MAXIMA cantidad de ocurrencias: {max(P5)} \n MINIMA cantidad de ocurrencias: {min(P5)} \n Media de ocurrencias: {stats.mean(P5)} \n Mediana de ocurrencias {stats.median(P5)} \n Moda de ocurrencias:{stats.mode(P5)} \n\n")
+    E.write(f"Palabra 6: {palabra6} \n Total de ocurrencias: {sum(P6)} \n MAXIMA cantidad de ocurrencias: {max(P6)} \n MINIMA cantidad de ocurrencias: {min(P6)} \n Media de ocurrencias: {stats.mean(P6)} \n Mediana de ocurrencias {stats.median(P6)} \n Moda de ocurrencias:{stats.mode(P6)} \n\n")
 """
 P1.reverse()
 print(P1)
