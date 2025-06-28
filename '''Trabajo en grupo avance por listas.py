6b5565c98498d941341c4ad6d062610620b8f434
SamuelCV06 <samuel.chinchilla@ucr.ac.cr>
2025-06-27 19:10:41 -0600

    pregunta

--- '''Trabajo en grupo avance por listas.py
+++ '''Trabajo en grupo avance por listas.py
@@ -53,19 +53,9 @@ for num_linea, linea in enumerate(lineas, start=1):
         p+=1
         
 
-with open("Estadísticas.txt", "w",) as E:
-    E.write(f"ESTADISTICAS DE PALABRAS \n Palabra 1: {palabra1} \n Total de ocurrencias: {sum(P1)} \n MAXIMA cantidad de ocurrencias: {max(P1)} \n MINIMA cantidad de ocurrencias: {min(P1)} \n Media de ocurrencias: {stats.mean(P1)} \n Mediana de ocurrencias {stats.median(P1)} \n Moda de ocurrencias:{stats.mode(P1)} \n\n ")
-with open("Estadísticas.txt", "a") as E: 
-    E.write(f"Palabra 2: {palabra2} \n Total de ocurrencias: {sum(P2)} \n MAXIMA cantidad de ocurrencias: {max(P2)} \n MINIMA cantidad de ocurrencias: {min(P2)} \n Media de ocurrencias: {stats.mean(P2)} \n Mediana de ocurrencias {stats.median(P2)} \n Moda de ocurrencias:{stats.mode(P2)} \n\n")
-    E.write(f"Palabra 3: {palabra3} \n Total de ocurrencias: {sum(P3)} \n MAXIMA cantidad de ocurrencias: {max(P3)} \n MINIMA cantidad de ocurrencias: {min(P3)} \n Media de ocurrencias: {stats.mean(P3)} \n Mediana de ocurrencias {stats.median(P3)} \n Moda de ocurrencias:{stats.mode(P3)} \n\n")
-    E.write(f"Palabra 4: {palabra4} \n Total de ocurrencias: {sum(P4)} \n MAXIMA cantidad de ocurrencias: {max(P4)} \n MINIMA cantidad de ocurrencias: {min(P4)} \n Media de ocurrencias: {stats.mean(P4)} \n Mediana de ocurrencias {stats.median(P4)} \n Moda de ocurrencias:{stats.mode(P4)} \n\n")
-    E.write(f"Palabra 5: {palabra5} \n Total de ocurrencias: {sum(P5)} \n MAXIMA cantidad de ocurrencias: {max(P5)} \n MINIMA cantidad de ocurrencias: {min(P5)} \n Media de ocurrencias: {stats.mean(P5)} \n Mediana de ocurrencias {stats.median(P5)} \n Moda de ocurrencias:{stats.mode(P5)} \n\n")
-    E.write(f"Palabra 6: {palabra6} \n Total de ocurrencias: {sum(P6)} \n MAXIMA cantidad de ocurrencias: {max(P6)} \n MINIMA cantidad de ocurrencias: {min(P6)} \n Media de ocurrencias: {stats.mean(P6)} \n Mediana de ocurrencias {stats.median(P6)} \n Moda de ocurrencias:{stats.mode(P6)} \n\n")
-
 N_mayor = []
 N_mayor.extend(P1), N_mayor.extend(P2), N_mayor.extend(P3), N_mayor.extend(P4), N_mayor.extend(P5), N_mayor.extend(P6)
 Mayor = max(N_mayor)
-print(Mayor)
 Fuente_t = {'family':'serif','color':'grey'} 
 Fuente_ax = {'family':'serif','color':'grey','size': 14} 
 fig, axes = plt.subplots(2,3,figsize=(Mayor,num_linea), sharex="row", sharey=all) 
@@ -97,4 +87,20 @@ ax.set_title(palabra6, color="magenta", fontdict = Fuente_ax)
 plt.suptitle("OCURRENCIAS DE PALABRAS POR PÁRRAFO", fontdict = Fuente_t, fontsize= 18, fontweight = "bold")
 plt.subplots_adjust(hspace=0.4, wspace=0.45)
 plt.show()
- 
+
+afirmación = ("S", "s")
+print("Desea salvar los valores estadísticos:")
+ARCHIVO = input("Digite S para confirmar:")
+if ARCHIVO in afirmación:
+    with open("Estadísticas.txt", "w",) as E:
+        E.write(f"ESTADISTICAS DE PALABRAS \n Palabra 1: {palabra1} \n Total de ocurrencias: {sum(P1)} \n MAXIMA cantidad de ocurrencias: {max(P1)} \n MINIMA cantidad de ocurrencias: {min(P1)} \n Media de ocurrencias: {stats.mean(P1)} \n Mediana de ocurrencias {stats.median(P1)} \n Moda de ocurrencias:{stats.mode(P1)} \n\n ")
+    with open("Estadísticas.txt", "a") as E: 
+        E.write(f"Palabra 2: {palabra2} \n Total de ocurrencias: {sum(P2)} \n MAXIMA cantidad de ocurrencias: {max(P2)} \n MINIMA cantidad de ocurrencias: {min(P2)} \n Media de ocurrencias: {stats.mean(P2)} \n Mediana de ocurrencias {stats.median(P2)} \n Moda de ocurrencias:{stats.mode(P2)} \n\n")
+        E.write(f"Palabra 3: {palabra3} \n Total de ocurrencias: {sum(P3)} \n MAXIMA cantidad de ocurrencias: {max(P3)} \n MINIMA cantidad de ocurrencias: {min(P3)} \n Media de ocurrencias: {stats.mean(P3)} \n Mediana de ocurrencias {stats.median(P3)} \n Moda de ocurrencias:{stats.mode(P3)} \n\n")
+        E.write(f"Palabra 4: {palabra4} \n Total de ocurrencias: {sum(P4)} \n MAXIMA cantidad de ocurrencias: {max(P4)} \n MINIMA cantidad de ocurrencias: {min(P4)} \n Media de ocurrencias: {stats.mean(P4)} \n Mediana de ocurrencias {stats.median(P4)} \n Moda de ocurrencias:{stats.mode(P4)} \n\n")
+        E.write(f"Palabra 5: {palabra5} \n Total de ocurrencias: {sum(P5)} \n MAXIMA cantidad de ocurrencias: {max(P5)} \n MINIMA cantidad de ocurrencias: {min(P5)} \n Media de ocurrencias: {stats.mean(P5)} \n Mediana de ocurrencias {stats.median(P5)} \n Moda de ocurrencias:{stats.mode(P5)} \n\n")
+        E.write(f"Palabra 6: {palabra6} \n Total de ocurrencias: {sum(P6)} \n MAXIMA cantidad de ocurrencias: {max(P6)} \n MINIMA cantidad de ocurrencias: {min(P6)} \n Media de ocurrencias: {stats.mean(P6)} \n Mediana de ocurrencias {stats.median(P6)} \n Moda de ocurrencias:{stats.mode(P6)} \n\n")
+    print("El Archivo de texto Estadisticas.txt a sido creado en la carpeta del programa.")
+    print("¡Gracias por usar este programa!")
+else:
+    print("¡Gracias por usar este programa!")
