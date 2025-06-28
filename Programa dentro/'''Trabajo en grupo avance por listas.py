3026dc1a309e19c7fdb1f4f84dae9b41d4815d24
SamuelCV06 <samuel.chinchilla@ucr.ac.cr>
2025-06-28 14:35:36 -0600

    final

--- Programa dentro/'''Trabajo en grupo avance por listas.py
+++ Programa dentro/'''Trabajo en grupo avance por listas.py
@@ -1,13 +1,52 @@
 '''Trabajo en grupo
+Algoritmo:
+1 Importamos las librerías necesarias
+2 Texto <---- perdirlo al usuario
+3 Líneas <--- leer líneas en el Texto
+4.Palabras <---- Pedir al usuario
+4.1 for palara in líneas en el Texto then
+4.1.1 Palabra_1_en_texto <---- cantidad de palabra 1 en texto 
+4.1.2 Palabra_2_en_texto <----- cantidad de palabra 2 en texto 
+4.1.3 Palabra_3_en_texto <---- cantidad de palabra 3 en texto
+4.1.4 Palabra_4_en_texto <---- cantidad de palabra 4 en texto
+4.1.5 Palabra_5_en_texto <---- cantidad de palabra 5 en texto
+4.1.6 Palabra_6_en_texto <---- cantidad de palabra 6 en texto
+4.1.7 número de líneas <---- número_de_linea
+5 Total_de_palabras_dentro_del_texto <--- palabra_1_en_texto, palabra_2_en_texto, palabra_3_en_texto, palabra_4_en_texto, palabra_5_en_texto, palabra_6_en_texto
+6 Número_más_grande <----- Máximo(Total_de_palabras_dentro_del_texto)
+7 fuente_título_principal <-- (Tipo de letra, color de letra, tamaño de letra, grueso de letra)
+8 fuente_título_grafica <-- (Tipo de letra, color de letra, tamaño de letra)
+9 definir_figura_graficos <--- modulo matplotlib (cantidad de subplots, tamaño de la figura, establecimiento de números para las cordenadas)
+9.1 ubicación grafica 1 <---- ubicación 
+9.2 Subplot1 <--- (cantidad de líneas, Palabra_1_en_texto, color)
+9.3 Subplit1_titulo <--- (palabra1, color, fuente_título_grafica)
+9.4 ubicación grafica 2 <---- ubicación 
+9.5 Subplot2 <--- (cantidad de líneas, Palabra_2_en_texto, color)
+9.6 Subplit2_titulo <--- (palabra1, color, fuente_titulo_grafica)
+9.7 ubicación grafica 3 <---- ubicación 
+9.8 Subplot3 <--- (cantidad de líneas, Palabra_3_en_texto, color)
+9.9 Subplit3_titulo <--- (palabra1, color, fuente_titulo_grafica)
+9.10 ubicación grafica 4 <---- ubicación 
+9.11 Subplot4 <--- (cantidad de líneas, Palabra_4_en_texto, color)
+9.12 Subplit4_titulo <--- (palabra1, color, fuente_titulo_grafica)
+9.13 ubicación grafica 5 <---- ubicación 
+9.14 Subplot5 <--- (cantidad de líneas, Palabra_5_en_texto, color)
+9.15 Subplit5_titulo <--- (palabra1, color, fuente_titulo_grafica)
+9.16 ubicación grafica 6 <---- ubicación 
+9.17 Subplot6 <--- (cantidad de líneas, Palabra_6_en_texto, color)
+9.18 Subplit6_titulo <--- (palabra1, color, fuente_titulo_grafica)
+10 print ("Desea salvar los valores estadísticos (S/N): ")
+11 respuesta <--- usuario
+12 if respuesta afirmativa then
+12.1 Estadísticas.txt <--- crear o abrir para escribir
+12.2 Estadísticas.txt <--- write ("ESTADISTICAS DE PALABRAS", estadísticas de cada palabra)
+12.3 print ("El Archivo de texto Estadisticas.txt ha sido creado en la carpeta del programa.")
+    print ("¡Gracias por usar este programa!")
+13 else
+13.1 print ("¡Gracias por usar este programa!")
 
-Proposito:
-
-Algoritmo
-
-1.abrir archivo
-2.texto <-- cargar archivo de texto
-3.palabras <-- pedir al usuario
-4.palabras <-- leer'''
+)
+'''
 
 #cargamos el archivo y leemos las lineas(parrafos) del texto
 #Import open file prompt
@@ -88,9 +127,9 @@ plt.suptitle("OCURRENCIAS DE PALABRAS POR PÁRRAFO", fontdict = Fuente_t, fontsi
 plt.subplots_adjust(hspace=0.4, wspace=0.45)
 plt.show()
 
-afirmación = ("S", "s")
-print("Desea salvar los valores estadísticos:")
-ARCHIVO = input("Digite S para confirmar:")
+afirmación = ("S", "s", "Si", "sI", "SI", "si", "Sí", "SÍ","sí", "sÍ")
+print("Desea salvar los valores estadísticos (S/N): ")
+ARCHIVO = input()
 if ARCHIVO in afirmación:
     with open("Estadísticas.txt", "w",) as E:
         E.write(f"ESTADISTICAS DE PALABRAS \n Palabra 1: {palabra1} \n Total de ocurrencias: {sum(P1)} \n MAXIMA cantidad de ocurrencias: {max(P1)} \n MINIMA cantidad de ocurrencias: {min(P1)} \n Media de ocurrencias: {stats.mean(P1)} \n Mediana de ocurrencias {stats.median(P1)} \n Moda de ocurrencias:{stats.mode(P1)} \n\n ")
